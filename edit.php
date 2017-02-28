1bffe2ac0a90d0fe3d50ebd2d73934db5a42ea20
Pavel Elagin <elagin.pasha@gmail.com>
2017-02-28 15:46:04 +0300

    Заработало добавление строки ингредиентов

--- edit.php
+++ edit.php
@@ -13,14 +13,20 @@ $method = new GetList($_GET);
 $recipe = $method->requestRecipe(1);
 $ingredientList = $recipe['i'];
 $stepList = $recipe['s'];
-
-//print_r($_POST);
+print_r($_POST);
 
 ?>
 
 <?php
-    function insertTextInput($name, $value) {
-        return ('<INPUT type="text" name="'. $name .'" maxlength="35" size="50" value="'. $value .'"/>');
+    function insertTextInput($name, $value, $size) {
+        //return ('<INPUT type="text" name="'. $name .'" maxlength="35" size="50" value="'. $value .'"/>');
+        return ('<INPUT type="text" name="'. $name .'" maxlength="'. $size .'" size="'. $size .'" value="'. $value .'"/>');
+}
+?>
+
+<?php
+    function insertTextarea($name, $value) {
+        return ('<textarea name="'. $name .'" cols="80" rows="5" >'. $value .'</textarea>');
 }
 ?>
 
@@ -38,34 +44,53 @@ $stepList = $recipe['s'];
     <?php
         if(count($ingredientList > 0))
         {
+            echo("\n");
             echo('<h3>Ингредиенты:</h3>');
-            echo('<table cellspacing="0" border="1" cellpadding="5" >');
+            echo('<table id="ingredientsTable" cellspacing="0" border="1" cellpadding="5" data-max_index=' .count($ingredientList). '>');
+            echo("<tr>");
+            echo('<td>Название</td><td>Объем</td><td>Количество</td>');
+            echo("</tr>");
+            echo("\n");
             for($i = 0; $i < count($ingredientList); ++$i)
             {
                 $rec = $ingredientList[$i];
                 echo("<tr data-index='$i'>");
-                echo('<td>'. insertTextInput("ingredient[$i][name]", $rec['name']) . '</td>');
-                echo('<td>'. insertTextInput("ingredient[$i][count]", $rec['count']) . '</td>');
-                echo('<td>'. insertTextInput("ingredient[$i][unit]", $rec['unit']) . '</td>');
-                echo('<td onclick="removeIngredient(this)">test</td>');
+                echo('<td>'. insertTextInput("ingredient[$i][name]", $rec['name'], 50) . '</td>');
+                echo('<td>'. insertTextInput("ingredient[$i][count]", $rec['count'], 5) . '</td>');
+                echo('<td>'. insertTextInput("ingredient[$i][unit]", $rec['unit'], 10) . '</td>');
+                echo('<td onclick="removeIngredient(this)">Удалить</td>');
                 echo("</tr>");
+                echo("\n");
             }
+/*            echo("<tr>");
+            echo('<td colspan="3" onclick="addIngredient(this)" align="center" >Добавить</td>');
+            echo("</tr>");*/
             echo("</table>");
+            echo("\n");
+            echo('<button type="button" onclick="addIngredient(this)">Добавить</button>');
         }
         if(count($stepList > 0))
         {
+            echo("\n");
             echo('<h3>Шаги:</h3>');
             echo('<table cellspacing="0" border="1" cellpadding="5" >');
+            echo("<tr>");
+            echo('<td>Описание</td><td>Время</td><td>Фото</td>');
+            echo("</tr>");
             for($i = 0; $i < count($stepList); ++$i)
             {
                 $rec = $stepList[$i];
                 echo("<tr data-index='$i'>");
-                echo('<td>'. insertTextInput("step[$i][desc]", $rec['desc']) . '</td>');
-                echo('<td>'. insertTextInput("step[$i][time]", $rec['time']) . '</td>');
-                echo('<td>'. insertTextInput("step[$i][photo]", $rec['photo']) . '</td>');
-                echo('<td onclick="removeStep(this)">test</td>');
+                echo('<td>'. insertTextarea("step[$i][desc]", $rec['desc']) . '</td>');
+                echo('<td>'. insertTextInput("step[$i][time]", $rec['time'], 5) . '</td>');
+                echo('<td>'. insertTextInput("step[$i][photo]", $rec['photo'], 10) . '</td>');
+                echo('<td onclick="removeStep(this)">Удалить</td>');
                 echo("</tr>");
+                echo("\n");
             }
+            echo("<tr>");
+            echo('<td colspan="3" onclick="addIngredient(this)" align="center" >Добавить</td>');
+            echo("</tr>");
             echo("</table>");
         }
     ?>
--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-/* 
+/* +
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 
-removeIngredient = function(el){
+removeIngredient = function (el) {
     el.parentNode.remove();
 };
 
-removeStep = function(el){
+removeStep = function (el) {
     el.parentNode.remove();
 };
+
+addIngredient = function () {
+    var idx = ingredientsTable.getAttribute('data-max_index');
+    var tr = document.createElement('tr');
+    var name = '<td><INPUT type="text" name=ingredient[' + idx + '][name] maxlength="50" size="50" value=""/></td>';
+    var count = '<td><INPUT type="text" name=ingredient[' + idx + '][count] maxlength="5" size="5" value=""/></td>';
+    var unit = '<td><INPUT type="text" name=ingredient[' + idx + '][unit] maxlength="10" size="10" value=""/></td>';
+    var del = '<td onclick="removeIngredient(this)">Удалить</td>';
+    var str = name + count + unit + del;
+    tr.innerHTML = str;
+    ingredientsTable.appendChild(tr);
+    ingredientsTable.setAttribute('data-max_index', idx++);
+}
